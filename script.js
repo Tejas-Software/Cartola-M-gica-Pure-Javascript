7d84c3b69dc9645dd8971353544311b0b749f6ba
unknown <luis.mendes.95@hotmail.com>
2024-02-08 11:59:28 -0300

    test without background to deploy update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { createBackground, insertImage, insertImageWithHoverAndLink } from "./content/functions.js";
 
-createBackground('/content/tela_01/cenario.svg');
+//createBackground('/content/tela_01/cenario.svg');
 insertImage('/content/tela_01/brand_logo.png', "2vh", "2vw", "0.4", "30vw", "10vh", "0");
 insertImage('/content/tela_01/nome_cartola.svg', "42vh", "17.5vw", "1", "60vw", "50vh", "998");
 insertImageWithHoverAndLink('/content/tela_01/botao_iniciar.svg', "5vh", "70vw", "1", "20vw", "10vh", "999");
