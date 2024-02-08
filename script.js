5f66c4cd94976eacb9146e9b1414a0b7c73b9b40
unknown <luis.mendes.95@hotmail.com>
2024-02-08 12:05:04 -0300

    try this position

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ import { createBackground, insertImage, insertImageWithHoverAndLink } from "./co
 createBackground('/content/tela_01/cenario.svg');
 insertImage('/content/tela_01/brand_logo.png', "2vh", "2vw", "0.4", "30vw", "10vh", "0");
 insertImage('/content/tela_01/nome_cartola.svg', "35vh", "17.5vw", "1", "60vw", "50vh", "998");
-insertImageWithHoverAndLink('/content/tela_01/botao_iniciar.svg', "5vh", "70vw", "1", "20vw", "10vh", "999");
-insertImage('/content/tela_01/cartas.svg', "10vh", "13vw", "1", "80vw", "50vh", "0",);
+insertImageWithHoverAndLink('/content/tela_01/botao_iniciar.svg', "2vh", "71vw", "1", "20vw", "10vh", "999");
+insertImage('/content/tela_01/cartas.svg', "6vh", "13vw", "1", "80vw", "50vh", "0",);
